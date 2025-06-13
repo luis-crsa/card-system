22eef20bd69bfb521ac823cbbd84ab58d7f8a822
Luis <luiscrsarmento@gmail.com>
2025-06-13 11:19:12 -0300

    projeto criado

---
+++ src/main/java/com/cesupa/cardsystem/CardsystemApplication.java
@@ -0,0 +1,13 @@
+package com.cesupa.cardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesupa/cardsystem/CardsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesupa.cardsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
