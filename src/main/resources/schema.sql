189b3651a27c9556274cf805a7b0512d04345abf
Ana-Beatriz-N <biafreirenunes2015@gmail.com>
2025-06-15 16:31:04 -0300

    Adição da coluna motivo_bloqueio

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS cartoes (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     cpf VARCHAR(11) NOT NULL,
     nome_completo VARCHAR(100) NOT NULL,
     data_nascimento DATE NOT NULL,
@@ -9,5 +9,6 @@ CREATE TABLE IF NOT EXISTS cartoes (
     tipo VARCHAR(20) NOT NULL,
     bandeira VARCHAR(20) NOT NULL,
     numero VARCHAR(20) NOT NULL,
-    status VARCHAR(30) NOT NULL
-);
+    status VARCHAR(30) NOT NULL,
+    motivo_bloqueio VARCHAR(255)
+    );
