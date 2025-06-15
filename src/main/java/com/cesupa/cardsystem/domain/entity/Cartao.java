0a533694014cab571dade0e0f34d1cf6d8ed0cc0
Ana-Beatriz-N <biafreirenunes2015@gmail.com>
2025-06-15 16:40:37 -0300

    Modifcação domain: entity: Cartao, enums: StatusCartao, repository: CartaoRepository

--- src/main/java/com/cesupa/cardsystem/domain/entity/Cartao.java
+++ src/main/java/com/cesupa/cardsystem/domain/entity/Cartao.java
@@ -20,8 +20,12 @@ public class Cartao {
     private BandeiraCartao bandeira;
     private String numero;
     private StatusCartao status;
+    private String motivoBloqueio;
 
-    public Cartao(UUID id, CPF cpf, String nomeCompleto, DataDeNascimento dataNascimento, RendaMensal rendaMensal, TipoCartao tipo, BandeiraCartao bandeira, String numero, StatusCartao status) {
+    public Cartao(UUID id, CPF cpf, String nomeCompleto, DataDeNascimento dataNascimento,
+                  RendaMensal rendaMensal, TipoCartao tipo, BandeiraCartao bandeira,
+                  String numero, StatusCartao status, String motivoBloqueio) {
+
         this.id = id;
         this.cpf = cpf;
         this.nomeCompleto = nomeCompleto;
@@ -31,8 +35,45 @@ public class Cartao {
         this.bandeira = bandeira;
         this.numero = numero;
         this.status = status;
+        this.motivoBloqueio = motivoBloqueio;
     }
 
+    public static Cartao solicitar(CPF cpf, String nomeCompleto, DataDeNascimento dataNascimento,
+                                   RendaMensal renda, TipoCartao tipo, BandeiraCartao bandeira){
+
+        if (!renda.atendeMinimoPara(tipo)) {
+            throw new IllegalArgumentException("Renda insuficiente para tipo de cartão solicitado.");
+        }
+
+        String numeroGerado = gerarNumero();
+
+        return new Cartao(
+                null,
+                cpf,
+                nomeCompleto,
+                dataNascimento,
+                renda,
+                tipo,
+                bandeira,
+                numeroGerado,
+                StatusCartao.SOLICITADO,
+                null // ← motivoBloqueio começa nulo
+        );
+    }
+
+    public void bloquearTemporariamente(String motivo) {
+        if (this.status != StatusCartao.ATIVO) {
+            throw new RuntimeException("Somente cartões ativos podem ser bloqueados temporariamente");
+        }
+        this.status = StatusCartao.BLOQUEADO_TEMPORARIO;
+        this.motivoBloqueio = motivo;
+    }
+
+    private static String gerarNumero() {
+        return String.valueOf((long) (Math.random() * 1_0000_0000_0000_0000L));
+    }
+
+    // Getters
     public UUID getId() {
         return id;
     }
@@ -69,33 +110,11 @@ public class Cartao {
         return status;
     }
 
+    public String getMotivoBloqueio() {
+        return motivoBloqueio;
+    }
+
     public void atribuirId(UUID id) {
         this.id = id;
-    }
-
-    public static Cartao solicitar(CPF cpf, String nomeCompleto, DataDeNascimento dataNascimento,
-                                   RendaMensal renda, TipoCartao tipo, BandeiraCartao bandeira){
-
-        if (!renda.atendeMinimoPara(tipo)) {
-            throw new IllegalArgumentException("Renda insuficiente para tipo de cartão solicitado.");
-        }
-
-        String numeroGerado = gerarNumero();
-
-        return new Cartao(
-                null,
-                cpf,
-                nomeCompleto,
-                dataNascimento,
-                renda,
-                tipo,
-                bandeira,
-                numeroGerado,
-                StatusCartao.SOLICITADO
-        );
-    }
-
-    private static String gerarNumero() {
-        return String.valueOf((long) (Math.random() * 1_0000_0000_0000_0000L));
     }
 }
