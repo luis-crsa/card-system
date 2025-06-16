aea4a0685fdf8cdc238404c18b2b04ee34331e60
Ana-Beatriz-N <biafreirenunes2015@gmail.com>
2025-06-15 23:41:29 -0300

    Conserto de Margem: resources: schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS cartoes (
-                                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     cpf VARCHAR(11) NOT NULL,
     nome_completo VARCHAR(100) NOT NULL,
     data_nascimento DATE NOT NULL,
